367b5f5bb13602009b4a1e10e99f206a40cb3afb
JDBernardi <joebernardi@hotmail.com>
2017-07-26 00:33:20 -0400

    set up assets and views

---
+++ BlockBreaker/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	public void LoadLevel(string name) {
+		Debug.Log ("level load requested:" + name +".");
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest(){
+		Debug.Log ("I want to quit");
+		Application.Quit (); //doesn't work on all platforms
+	}
+}
